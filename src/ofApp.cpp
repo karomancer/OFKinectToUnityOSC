baacdf71203d41a653c54dae20407c6ccacfd0ed
Karina Chow <karinachowtime@gmail.com>
2023-11-26 00:39:54 -0500

    Finally got it working.
    Persistence still isn't great

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,38 +13,40 @@ void ofApp::setup()
     
     // Set up canvas
     ofBackground(255);
+    
     drawBounds.set(0, 0, KINECT_DEPTH_WIDTH, KINECT_DEPTH_HEIGHT);
     drawBounds.scaleTo(ofGetCurrentViewport(), OF_SCALEMODE_FILL);
-    
+
     // Set up OSC sender
     oscSender.setup("localhost", 1337);
-    
+
     // Kinect GUI options
     kinectGuiGroup.setup("Kinect");
     kinectGuiGroup.add(showDepthMap.set("Show Kinect Depth Map", false));
-    kinectGuiGroup.add(minDepth.set("Min depth", 0.5f, 0.5f, 10.f));
-    kinectGuiGroup.add(maxDepth.set("Max depth", 1.7f, 0.5f, 15.f));
+    kinectGuiGroup.add(minDepth.set("Min depth", 0.5f, 0.5f, 8.f));
+    kinectGuiGroup.add(maxDepth.set("Max depth", 5.f, 0.5f, 8.f));
     kinectGuiGroup.add(anchorDepth.set("Base pixel size", 1, 1, 5));
+
+    // Contour finder GUI options
+    contourFinderGuiGroup.setup("Contour Finder");
+    contourFinderGuiGroup.add(minContourArea.set("Min area", 0.01f, 0, 100.f));
+    contourFinderGuiGroup.add(maxContourArea.set("Max area", 0.4f, 0, 300.f));
+    contourFinderGuiGroup.add(persistence.set("Persistence", 15, 0, 300));
+
+    // Set up GUI panel
+    guiPanel.setup("SETTINGS", "settings.json");
+    guiPanel.add(&kinectGuiGroup);
+    guiPanel.add(&contourFinderGuiGroup);
+    guiPanel.loadFromFile("settings.json");
     
     // Set up Kinect
     ofxKinectV2::Settings kinectSettings;
-    kinectSettings.enableRGB = true;
-    kinectSettings.enableIR = true;
+    kinectSettings.enableRGB = false;
+    kinectSettings.enableIR = false;
     kinectSettings.enableRGBRegistration = false;
-    kinectSettings.config.MinDepth = minDepth;
-    kinectSettings.config.MaxDepth = maxDepth;
+//    kinectSettings.config.MinDepth = minDepth;
+//    kinectSettings.config.MaxDepth = maxDepth;
     kinect.open(0, kinectSettings);
-    
-    // Contour finder GUI options
-    contourFinderGuiGroup.setup("Contour Finder");
-    contourFinderGuiGroup.add(minContourArea.set("Min area", 0.01f, 0, 10.f));
-    contourFinderGuiGroup.add(maxContourArea.set("Max area", 0.4f, 0, 50.f));
-    contourFinderGuiGroup.add(persistence.set("Persistence", 15, 0, 300));
-    
-    // Set up GUI panel
-    guiPanel.setup("People Detection Settings", "settings.json");
-    guiPanel.add(&kinectGuiGroup);
-    guiPanel.add(&contourFinderGuiGroup);
 }
 
 void ofApp::update()
@@ -59,33 +61,33 @@ void ofApp::update()
         
         canvasFbo.allocate(KINECT_DEPTH_WIDTH, KINECT_DEPTH_HEIGHT);
         visionFbo.allocate(KINECT_DEPTH_WIDTH, KINECT_DEPTH_HEIGHT);
-        
+
         // Drawing the canvas
         canvasFbo.begin();
-        ofClear(255);
-        ofSetColor(ofColor::black);
-        ofFill();
-        for (int y = 0; y < depthPixels.getHeight(); y += 4) {
-            for (int x = 0; x < depthPixels.getWidth(); x += 4) {
+        for (int y = 0; y < depthPixels.getHeight(); y+=2) {
+            for (int x = 0; x < depthPixels.getWidth(); x+=2) {
                 float dist = kinect.getDistanceAt(x, y);
-                
+
                 if (dist > minDepth && dist < maxDepth) {
+                    int grayInt = ofMap(dist, minDepth, maxDepth, 255, 0);
                     float radius = ofMap(dist, minDepth, maxDepth, anchorDepth * 2, 0);
+                    ofSetColor(ofColor(grayInt, grayInt, grayInt));
+                    ofFill();
                     ofDrawCircle(x + anchorDepth, y + anchorDepth, radius);
                 }
             }
         }
         canvasFbo.end();
-        
+
         canvasFbo.readToPixels(canvasPixels);
-        
+
         // Drawing the contour
-        contourFinder.setMinAreaNorm(minContourArea);
-        contourFinder.setMaxAreaNorm(maxContourArea);
+        contourFinder.setMinAreaRadius(minContourArea);
+        contourFinder.setMaxAreaRadius(maxContourArea);
         contourFinder.getTracker().setPersistence(persistence);
-        contourFinder.findContours(canvasPixels);
+        contourFinder.findContours(showDepthMap ? depthPixels : canvasPixels);
         std::vector<cv::Rect> blobs = contourFinder.getBoundingRects();
-//        cout << "num blobs " << blobs.size() << endl;
+        cout << "num blobs " << blobs.size() << endl;
         
         // Draw the contour in its own FBO
         visionFbo.begin();
@@ -111,9 +113,9 @@ void ofApp::draw()
         ofFill();
         depthTex.draw(drawBounds);
     } else {
-//        canvasFbo.draw(drawBounds);
+        canvasFbo.draw(drawBounds);
     }
     
-//    visionFbo.draw(drawBounds);
+    visionFbo.draw(drawBounds);
     guiPanel.draw();
 }
