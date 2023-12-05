9891618d476c100ee34c441a4cd460d29bc4d804
Karina Chow <karinachowtime@gmail.com>
2023-12-04 23:55:48 -0500

    WIP

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,6 +1,9 @@
 // ofApp.cpp
 #include "ofApp.h"
 
+const int KINECT_DEPTH_WIDTH = 512;
+const int KINECT_DEPTH_HEIGHT = 424;
+
 void ofApp::setup()
 {
     int width = ofGetScreenWidth();
@@ -10,6 +13,9 @@ void ofApp::setup()
     // Set up canvas
     ofBackground(255);
     
+    drawBounds.set(0, 0, KINECT_DEPTH_WIDTH, KINECT_DEPTH_HEIGHT);
+    drawBounds.scaleTo(ofGetCurrentViewport(), OF_SCALEMODE_FILL);
+    
     // Set up frame buffer
     ofFboSettings fboSettings;
     fboSettings.width = width;
@@ -18,25 +24,33 @@ void ofApp::setup()
     
     // Set up GUI panel
     guiPanel.setup("DEPTH_DOTS", "settings.json");
+    minIR.set("Min IR value", 0.5f, 0.f, 1.f);
+    maxIR.set("Max IR value", 0.5f, 0.f, 1.f);
     minDepth.set("Min depth", 0.5f, 0.5f, 8.f);
     maxDepth.set("Max depth", 1.3f, 0.5f, 8.f);
+    millisToClear.set("Ms to clear", 200, 200, 10000);
+    anchorDepth.set("Base pixel size", 1.f, 1.f, 10.f);
     
-    anchorDepth.set("Base pixel size", .75f, 0.f, 5.f);
-        
     showDepthMap.set("Show Kinect Depth Map", false);
+    showIRMap.set("Show Kinect IR Map", false);
     
     guiPanel.add(showDepthMap);
+    guiPanel.add(showIRMap);
     guiPanel.add(minDepth);
     guiPanel.add(maxDepth);
+    guiPanel.add(minIR);
+    guiPanel.add(maxIR);
     guiPanel.add(anchorDepth);
-        
+    guiPanel.add(millisToClear);
+    guiPanel.loadFromFile("settings.json");
+    
     // Set up Kinect
     ofxKinectV2::Settings kinectSettings;
     kinectSettings.enableRGB = false;
-    kinectSettings.enableIR = false;
+    kinectSettings.enableIR = true;
     kinectSettings.enableRGBRegistration = false;
-    kinectSettings.config.MinDepth = minDepth;
     kinectSettings.config.MaxDepth = maxDepth;
+    kinectSettings.config.MinDepth = minDepth;
     kinect.open(0, kinectSettings);
 }
 
@@ -50,45 +64,57 @@ void ofApp::update()
         depthPixels = kinect.getDepthPixels();
         depthTex.loadData(depthPixels);
         
-        canvasFbo.begin();
-        ofSetColor(ofColor(0, 0, 0));
-        ofFill();
-        float xMultiplier = (float) ofGetScreenWidth() / depthPixels.getWidth();
-        float yMultiplier = (float) ofGetScreenHeight() / depthPixels.getHeight() + 0.05;
+        irPixels = kinect.getIRPixels();
+        irTex.loadData(irPixels);
         
-        for (int y = 0; y < depthPixels.getHeight(); y++) {
-            for (int x = 0; x < depthPixels.getWidth(); x++) {
-                float dist = kinect.getDistanceAt(x, y);
-                
-                if (dist > minDepth && dist < maxDepth) {
-                    float radius = ofMap(dist, minDepth, maxDepth, anchorDepth + 1, 1);
-                    ofDrawCircle(x * xMultiplier + anchorDepth, y * yMultiplier + anchorDepth, radius);
+        canvasFbo.begin();
+        ofEnableAlphaBlending();
+        if (ofGetElapsedTimeMillis() - ellapsedMillisSinceClear > millisToClear) {
+            ellapsedMillisSinceClear = ofGetElapsedTimeMillis();
+            ofClear(255);
+            cout << "Clear" << endl;
+        } else {
+            ofSetColor(ofColor::black);
+            ofFill();
+            
+            for (int y = 0; y < depthPixels.getHeight(); y++) {
+                for (int x = 0; x < depthPixels.getWidth(); x++) {
+                    float dist = kinect.getDistanceAt(x, y);
+                    float ir = irPixels[x + (irTex.getHeight() * y)];
+                    
+                    if (dist > minDepth && dist < maxDepth) {
+                        float radius = ofMap(dist, minDepth, maxDepth, anchorDepth * 2, 0);
+                        ofSetColor(ofColor::black);
+                        ofFill();
+                        ofDrawCircle(x + anchorDepth, y + anchorDepth, radius);
+                    }
                 }
             }
         }
+        ofEnableAlphaBlending();
+        ofSetColor(255, 2);
+        ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
         canvasFbo.end();
+        
+        ofPixels canvasPixels;
+        canvasFbo.readToPixels(canvasPixels);
+        canvasTexture.loadData(canvasPixels);
     }
+    
 }
 
 void ofApp::draw()
 {
-    if (showDepthMap) {
+    if (showIRMap) {
         ofSetColor(255);
         ofFill();
         
-        depthTex.draw(ofGetScreenWidth()/2 - depthPixels.getWidth()/2, ofGetScreenHeight()/2 - depthPixels.getHeight()/2);
-        
-        // Get the point distance using the SDK function (in meters).
-        float distAtMouse = kinect.getDistanceAt(ofGetMouseX(), ofGetMouseY());
-        ofDrawBitmapStringHighlight(ofToString(distAtMouse, 3), ofGetMouseX(), ofGetMouseY() - 10);
-        
-        // Get the point depth using the texture directly (in millimeters).
-        const ofFloatPixels& rawDepthPix = kinect.getRawDepthPixels();
-        int depthAtMouse = rawDepthPix.getColor(ofGetMouseX(), ofGetMouseY()).r;
-        ofDrawBitmapStringHighlight(ofToString(depthAtMouse), ofGetMouseX() + 16, ofGetMouseY() + 10);
+        irTex.draw(0, 0, ofGetScreenWidth(), ofGetScreenHeight());
+    } else if (showDepthMap) {
+        depthTex.draw(0, 0, ofGetScreenWidth(), ofGetScreenHeight());
     }
     else {
-        canvasFbo.draw(0, 0);
+        canvasFbo.draw(drawBounds);
     }
     
     guiPanel.draw();
